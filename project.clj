408881882ee1d350ebcab33003f7c22eee30e192
Zach Tellman <ztellman@gmail.com>
2013-07-30 21:28:06 -0700

    mark 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject immutable-bitset "0.1.2-SNAPSHOT"
+(defproject immutable-bitset "0.1.2"
   :description "a bitset which is immutable"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
