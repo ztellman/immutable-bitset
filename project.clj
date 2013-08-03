d62a633387530af8fdd47cef87bafe1ea0867487
Zach Tellman <ztellman@gmail.com>
2013-08-03 15:30:29 -0700

    mark 0.1.3

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject immutable-bitset "0.1.3-SNAPSHOT"
+(defproject immutable-bitset "0.1.3"
   :description "a bitset which is immutable"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[primitive-math "0.1.2"]]
+  :dependencies [[primitive-math "0.1.3"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [criterium "0.4.1"]
-                                  [reiddraper/simple-check "0.2.0"]]}}
+                                  [reiddraper/simple-check "0.2.1"]]}}
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
   :jvm-opts ^:replace ["-server"])
